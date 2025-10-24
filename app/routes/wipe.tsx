e0f981a628b7a9b8ff09623a590305f2f5851e42
Nishia Pinlac <pinlacnishia@gmail.com>
2025-10-24 22:54:00 +0800

    first commit

---
+++ app/routes/wipe.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
+const WipeApp = () => {
+    const {isLoading, error, clearError, fs, ai, kv } = usePuterStore();
+    const navigate = useNavigate();
+    const [files, setFiles] = useState<FSItem[]>([]);
+
+    const loadFiles = async () => {
+        const files = (await fs.readDir("./")) as FSItem[];
+        setFiles(files);
+    };
+
+    useEffect(() => {
+        loadFiles();
+    }, []);
+
+    const handleDelete = async () => {
+        files.forEach(async (file) => {
+            await fs.delete(file.path);
+        });
+        await kv.flush();
+        loadFiles();
+    };
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error {error}</div>;
+    }
+
+    return (
+    <div>
+            <div>Existing files:</div>
+            <div className="flex flex-col gap-4">
+                {files.map((file) => (
+                    <div key={file.id} className="flex flex-row gap-4">
+                        <p>{file.name}</p>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <button
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                    onClick={() => handleDelete()}
+                >
+                    Wipe App Data
+                </button>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default WipeApp;
