28a16128b1326547536b19d8ed5e8dcdc319f45c
Nishia Pinlac <pinlacnishia@gmail.com>
2025-10-24 23:30:22 +0800

    dx

--- app/components/feedback/Summary.tsx
+++ app/components/feedback/Summary.tsx
@@ -1,28 +1,5 @@
-
-import ScoreGauge from "../ScoreGauge";
-
-const ScoreBadge = ({ score }: { score: number }) => {
-  const badgeColor =
-    score > 69
-      ? "bg-badge-green"
-      : score > 49
-      ? "bg-badge-yellow"
-      : "bg-badge-red";
-  const textColor =
-    score > 69
-      ? "text-green-600"
-      : score > 49
-      ? "text-yellow-600"
-      : "text-red-600";
-  const badgeText =
-    score > 69 ? "Strong" : score > 49 ? "Good Start" : "Needs Work";
-
-    return (
-    <div className={`score-badge ${badgeColor}`}>
-      <p className={`text-xs ${textColor} font-semibold`}>{badgeText}</p>
-    </div>
-  );
-};
+import ScoreGauge from "~/components/ScoreGauge";
+import ScoreBadge from "~/components/ScoreBadge";
 
 const Category = ({ title, score }: { title: string; score: number }) => {
   const textColor =
